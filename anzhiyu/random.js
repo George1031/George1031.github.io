b09a8736d95cc296067f9cc6d25f21058c90b38a
george <zhangxu121213@163.com>
2024-05-17 17:46:44 +0800

    Site updated: 2024-05-17 17:46:43

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/07/hello-world/","2024/05/07/纯小白搭建博客/","2024/05/17/scrt安装激活/"];function toRandomPost(){
+var posts=["2024/05/07/hello-world/","2024/05/17/scrt安装激活/","2024/05/07/纯小白搭建博客/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
